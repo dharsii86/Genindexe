3b79961f29e5adca3d6d36c4c6f4c0eb416feb5f
tdequipe <totoddu94@gmail.com>
2016-05-11 12:00:17 +0200

    Add test java

---
+++ Test Package/database/CustomerDBTest.java
@@ -0,0 +1,110 @@
+package database;
+
+import database.ConnectionDB;
+import database.CustomerDB;
+import nf.Customer;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Manage the test of the CustomerDB class.
+ *
+ * @author SCRUM Group 2.
+ */
+public class CustomerDBTest {
+
+    public CustomerDBTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addCustomer method, of class CustomerDB. Test the insertion of
+     * the customers.
+     */
+    @Test
+    public void testAddCustomer() {
+        System.out.println("addCustomer");
+
+        // Creation of a Customer instance
+        Customer newCustomer = new Customer("Bidule", "London");
+
+        // Number of customer before insertion
+        int resBefore = Integer.parseInt(ConnectionDB.requestOneResult("select count(*) from customer"));
+        //System.out.println(resAvant);
+
+        // Creation of a CustomerDB instance
+        CustomerDB instance = new CustomerDB();
+        // Insertion of the customer
+        instance.addCustomer(newCustomer);
+
+        // Number of customer after insertion
+        int resAfter = Integer.parseInt(ConnectionDB.requestOneResult("select count(*) from customer"));
+        //System.out.println(resApres);
+
+        if (resAfter == resBefore) {
+            fail("Customer not added.");
+        }
+    }
+
+    /**
+     * Test of addCustomer method, of class CustomerDB. Test the insertion of
+     * duplicates.
+     */
+    @Test
+    public void testAddCustomerDuplicates() {
+        System.out.println("addCustomerDuplicates");
+
+        // Creation of two customers with the same name and the same town
+        Customer newCustomer1 = new Customer("Parrots Academy", "London");
+        Customer newCustomer2 = new Customer("PARROTS ACADEMY", "London");
+
+        // Creation of a CustomerDB instance
+        CustomerDB instance = new CustomerDB();
+        // Insertion of the customers
+        instance.addCustomer(newCustomer1);
+        instance.addCustomer(newCustomer2);
+
+        // Research of duplicates
+        ConnectionDB cDB = new ConnectionDB();
+        ResultSet res = cDB.request("select * from customer");
+
+        int cpt = 0;
+
+        try {
+            while (res.next()) {
+
+                if (res.getString("Cust_companyName").equalsIgnoreCase(newCustomer1.getName())
+                        && res.getString("Cust_town").equalsIgnoreCase(newCustomer1.getTown())) {
+                    cpt++;
+                }
+            }
+        } catch (SQLException ex) {
+        }
+        cDB.delete();
+
+        if (cpt > 1) {
+            fail("Duplicates allowed.");
+        }
+    }
+}
