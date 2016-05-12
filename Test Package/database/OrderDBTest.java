4ac7b5e28466139f6ac464f389157de0217e1fce
nvilleriot <nicolas.villeriot@gmail.com>
2016-05-12 18:16:24 +0200

    US2 Test for the OrderDB class commented and completed without the new modifications of the US2 by JS and Bob.

--- Test Package/database/OrderDBTest.java
+++ Test Package/database/OrderDBTest.java
@@ -44,13 +44,19 @@ public class OrderDBTest {
      */
     @Test
     public void testGetCustomerTown() {
-        ConnectionDB.requestInsert("delete from customer where Customer_Name = 'Machin' or Customer_Name = 'Truc' or Customer_Name = 'Bidule'");
+        //Delete in the database information which can hinder the test
+        ConnectionDB.requestInsert("delete from customer where Customer_Name = 'Machin' or Customer_Name = 'Truc' or Customer_Name = 'Bidule' or Customer_Name = 'What'");
+        //Insert in the database information needed for the test
+        ConnectionDB.requestInsert("insert into customer (Customer_Name,Customer_Town) values('Machin','What')");
         ConnectionDB.requestInsert("insert into customer (Customer_Name,Customer_Town) values('Machin','Town')");
         ConnectionDB.requestInsert("insert into customer (Customer_Name,Customer_Town) values('Truc','Town')");
         ConnectionDB.requestInsert("insert into customer (Customer_Name,Customer_Town) values('Bidule','Nope')");
+        //Try the tested method and collect results
         String[] listCustomerTown = OrderDB.getCustomerTown();
-        ConnectionDB.requestInsert("delete from customer where Customer_Name = 'Machin' or Customer_Name = 'Truc' or Customer_Name = 'Bidule'");
-        if (listCustomerTown.length != 2){
+        //Delete in the database inforamtion used for the test
+        ConnectionDB.requestInsert("delete from customer where Customer_Name = 'Machin' or Customer_Name = 'Truc' or Customer_Name = 'Bidule' or Customer_Name = 'What'");
+        //Test the results
+        if (listCustomerTown.length != 3){
             fail("The function getCustomerTown() doesn't return the good results!");
         }
     }
@@ -60,12 +66,17 @@ public class OrderDBTest {
      */
     @Test
     public void testGetCustomerName() {
+        //Delete in the database information which can hinder the test
         ConnectionDB.requestInsert("delete from customer where Customer_Name = 'Machin' or Customer_Name = 'Truc' or Customer_Name = 'Bidule'");
+        //Insert in the database information needed for the test
         ConnectionDB.requestInsert("insert into customer (Customer_Name,Customer_Town) values('Machin','Town')");
         ConnectionDB.requestInsert("insert into customer (Customer_Name,Customer_Town) values('Truc','Town')");
         ConnectionDB.requestInsert("insert into customer (Customer_Name,Customer_Town) values('Bidule','Nope')");
+        //Try the tested method and collect results
         String[] listCustomer = OrderDB.getCustomerName("Town");
+        //Delete in the database inforamtion used for the test
         ConnectionDB.requestInsert("delete from customer where Customer_Name = 'Machin' or Customer_Name = 'Truc' or Customer_Name = 'Bidule'");
+        //Test the results
         if (listCustomer.length != 2){
             fail("The function getCustomerTown() doesn't return the good results!");
         }
@@ -76,12 +87,17 @@ public class OrderDBTest {
      */
     @Test
     public void testGetCategory() {
+        //Delete in the database information which can hinder the test
         ConnectionDB.requestInsert("delete from category where Category_Name = 'Machin' or Category_Name = 'Truc' or Category_Name = 'Bidule'");
+        //Insert in the database information needed for the test
         ConnectionDB.requestInsert("insert into category values ('Machin')");
         ConnectionDB.requestInsert("insert into category values ('Truc')");
         ConnectionDB.requestInsert("insert into category values ('Bidule')");
+        //Try the tested method and collect results
         String[] listCategory = OrderDB.getCategory();
+        //Delete in the database inforamtion used for the test
         ConnectionDB.requestInsert("delete from category where Category_Name = 'Machin' or Category_Name = 'Truc' or Category_Name = 'Bidule'");
+        //Test the results
         if (listCategory.length != 3){
             fail("The function getCategory() doesn't return the good results!");
         }
@@ -92,16 +108,22 @@ public class OrderDBTest {
      */
     @Test
     public void testGetSpecies() {
+        //Delete in the database information which can hinder the test
         ConnectionDB.requestInsert("delete from specie where Specie_Name = 'Lolo' or Specie_Name = 'Lala' or Specie_Name = 'Machin'");
-        ConnectionDB.requestInsert("delete from category where Category_Name = 'Truc'");
+        ConnectionDB.requestInsert("delete from category where Category_Name = 'Truc' or Category_Name = 'Trac'");
+        //Insert in the database information needed for the test
         ConnectionDB.requestInsert("insert into category values('Truc')");
-        ConnectionDB.requestInsert("insert into specie values('Lolo','Truc')");
+        ConnectionDB.requestInsert("insert into category values('Trac')");
+        ConnectionDB.requestInsert("insert into specie values('Lolo','Trac')");
         ConnectionDB.requestInsert("insert into specie values('Lala','Truc')");
         ConnectionDB.requestInsert("insert into specie values('Machin','Truc')");
+        //Try the tested method and collect results
         String[] listSpecies = OrderDB.getSpecies("Truc");
+        //Delete in the database inforamtion used for the test
         ConnectionDB.requestInsert("delete from specie where Specie_Name = 'Lolo' or Specie_Name = 'Lala' or Specie_Name = 'Machin'");
-        ConnectionDB.requestInsert("delete from category where Category_Name = 'Truc'");
-        if (listSpecies.length != 3){
+        ConnectionDB.requestInsert("delete from category where Category_Name = 'Truc' or Category_Name = 'Trac'");
+        //Test the results
+        if (listSpecies.length != 2){
             fail("The function getSpecies() doesn't return the good results!");
         }
     }
@@ -111,21 +133,29 @@ public class OrderDBTest {
      */
     @Test
     public void testGetAnalysis() {
-        ConnectionDB.requestInsert("delete from relevant where Analysis_Name = 'Lolo' or Analysis_Name = 'Lala'");
-        ConnectionDB.requestInsert("delete from analysis where Analysis_Name = 'Lolo' or Analysis_Name = 'Lala'");
-        ConnectionDB.requestInsert("delete from specie where Specie_Name = 'Truc'");
+        //Delete in the database information which can hinder the test
+        ConnectionDB.requestInsert("delete from relevant where Analysis_Name = 'Lolo' or Analysis_Name = 'Lala' or Analysis_Name = 'Lulu'");
+        ConnectionDB.requestInsert("delete from analysis where Analysis_Name = 'Lolo' or Analysis_Name = 'Lala' or Analysis_Name = 'Lulu'");
+        ConnectionDB.requestInsert("delete from specie where Specie_Name = 'Truc' or Specie_Name = 'Trac'");
         ConnectionDB.requestInsert("delete from category where Category_Name = 'Machin'");
+        //Insert in the database information needed for the test
         ConnectionDB.requestInsert("insert into category values('Machin')");
         ConnectionDB.requestInsert("insert into specie values('Truc','Machin')");
+        ConnectionDB.requestInsert("insert into specie values('Trac','Machin')");
         ConnectionDB.requestInsert("insert into analysis values('Lolo')");
         ConnectionDB.requestInsert("insert into relevant values('Truc','Lolo')");
         ConnectionDB.requestInsert("insert into analysis values('Lala')");
         ConnectionDB.requestInsert("insert into relevant values('Truc','Lala')");
+        ConnectionDB.requestInsert("insert into analysis values('Lulu')");
+        ConnectionDB.requestInsert("insert into relevant values('Trac','Lulu')");
+        //Try the tested method and collect results
         String[] listAnalysis = OrderDB.getAnalysis("Truc");
-        ConnectionDB.requestInsert("delete from relevant where Analysis_Name = 'Lolo' or Analysis_Name = 'Lala'");
-        ConnectionDB.requestInsert("delete from analysis where Analysis_Name = 'Lolo' or Analysis_Name = 'Lala'");
-        ConnectionDB.requestInsert("delete from specie where Specie_Name = 'Truc'");
+        //Delete in the database inforamtion used for the test
+        ConnectionDB.requestInsert("delete from relevant where Analysis_Name = 'Lolo' or Analysis_Name = 'Lala' or Analysis_Name = 'Lulu'");
+        ConnectionDB.requestInsert("delete from analysis where Analysis_Name = 'Lolo' or Analysis_Name = 'Lala' or Analysis_Name = 'Lulu'");
+        ConnectionDB.requestInsert("delete from specie where Specie_Name = 'Truc' or Specie_Name = 'Trac'");
         ConnectionDB.requestInsert("delete from category where Category_Name = 'Machin'");
+        //Test the results
         if (listAnalysis.length != 2){
             fail("The function getAnalysis() doesn't return the good results!");
         }
@@ -136,16 +166,20 @@ public class OrderDBTest {
      */
     @Test
     public void testFormatResult() {
+        //Creation of three Arraylist of one element
         ArrayList list1 = new ArrayList();
         list1.add("Machin");
         ArrayList list2 = new ArrayList();
         list2.add("Truc");
         ArrayList list3 = new ArrayList();
         list3.add("Bidule");
+        //Creation of an ArrayList listGlobal which contains the three previous ArrayList for the test
+        //This listGlobal is in the format received by the tested method
         ArrayList listGlobal = new ArrayList();
         listGlobal.add(list1);
         listGlobal.add(list2);
         listGlobal.add(list3);
+        //Method test
         try{
             String[] listResult = OrderDB.formatResult(listGlobal);
             if (!listResult[0].equals("Machin") && !listResult[1].equals("Truc") && !listResult[2].equals("Bidule")){
@@ -157,3 +191,4 @@ public class OrderDBTest {
     }
     
 }
+
