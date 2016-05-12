066dc2b4adc8a6298b3dbab3bf7df5935b0b114e
tdequipe <totoddu94@gmail.com>
2016-05-12 11:29:39 +0200

    Description SpecieDB

--- Source Package/database/SpecieDB.java
+++ Source Package/database/SpecieDB.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package database;
+
 import nf.Specie;
 
 /**
@@ -11,5 +7,41 @@ import nf.Specie;
  * @author JCGx
  */
 public class SpecieDB {
-    public void addSpecie(Specie S){}
+
+    /**
+     * SpecieDB class constructor.
+     */
+    public SpecieDB() {
+    }
+
+    /**
+     * Add a specie in the database.
+     *
+     * @param spe, the specie to add.
+     * @return true if the specie is added and false if not.
+     */
+    public static boolean addSpecie(Specie spe) {
+        return true;
+    }
+
+    /**
+     * Check if the specie has a duplicate in the database.
+     *
+     * @param spe, the specie to check.
+     * @return true if there is duplicate and false if not.
+     */
+    public static boolean checkSpecieDuplicates(Specie spe) {
+        return true;
+    }
+
+    /**
+     * Check if the specie's format is correct.
+     *
+     * @param spe, the specie to check.
+     * @return true if the format is correct and false if not.
+     */
+    public static boolean checkSpecieFormat(Specie spe) {
+        return true;
+    }
+
 }
