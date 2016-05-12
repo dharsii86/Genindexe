a1750dbf2fa72db07bb7d4f887ffb5e998e6209c
TheGteck <quentin.bonenfant@gmail.com>
2016-05-12 09:10:16 +0200

    Test de correction du nom de table  Customer

--- Source Package/database/CustomerDB.java
+++ Source Package/database/CustomerDB.java
@@ -24,7 +24,7 @@ public class CustomerDB {
 
         if (this.checkCustomerDuplicates(cust)) {
 
-            ConnectionDB.requestInsert("insert into `customer` (`Customer_Name`, `Customer_Town`) values ('" + cust.getName() + "','" + cust.getTown() + "')");
+            ConnectionDB.requestInsert("insert into `Customer` (`Customer_Name`, `Customer_Town`) values ('" + cust.getName() + "','" + cust.getTown() + "')");
             //System.out.println("The customer has been added to the database");
             return true;
         }
@@ -45,7 +45,7 @@ public class CustomerDB {
             String n = cust.getName().toUpperCase();
             String t = cust.getTown().toUpperCase();
 
-            int resultat = Integer.parseInt(ConnectionDB.requestOneResult("select count(*) from customer where Customer_Name = '" + n + "' and Customer_Town = '" + t + "'"));
+            int resultat = Integer.parseInt(ConnectionDB.requestOneResult("select count(*) from Customer where Customer_Name = '" + n + "' and Customer_Town = '" + t + "'"));
 
             switch (resultat) {
                 case 0:
