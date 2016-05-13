550b0ed1110c7f5f3acee88326f07fffbb2ec49f
dharsii86 <guepard86@orange.fr>
2016-05-13 09:09:38 +0200

    majuscule et bouger deux fct

--- Source Package/database/CustomerDB.java
+++ Source Package/database/CustomerDB.java
@@ -1,5 +1,7 @@
 package database;
 
+import static database.OrderDB.formatResult;
+import java.util.ArrayList;
 import nf.Customer;
 
 /**
@@ -24,7 +26,7 @@ public class CustomerDB {
 
         if (CustomerDB.checkCustomerDuplicates(cust)) {
 
-            ConnectionDB.requestInsert("insert into `Customer` (`Customer_Name`, `Customer_Town`) values ('" + cust.getName() + "','" + cust.getTown() + "')");
+            ConnectionDB.requestInsert("insert into `customer` (`customer_name`, `customer_town`) values ('" + cust.getName() + "','" + cust.getTown() + "')");
             //System.out.println("The customer has been added to the database");
             return true;
         }
@@ -45,7 +47,7 @@ public class CustomerDB {
             String n = cust.getName().toUpperCase();
             String t = cust.getTown().toUpperCase();
 
-            int resultat = Integer.parseInt(ConnectionDB.requestOneResult("select count(*) from Customer where Customer_Name = '" + n + "' and Customer_Town = '" + t + "'"));
+            int resultat = Integer.parseInt(ConnectionDB.requestOneResult("select count(*) from customer where customer_name = '" + n + "' and customer_town = '" + t + "'"));
 
             switch (resultat) {
                 case 0:
@@ -57,4 +59,39 @@ public class CustomerDB {
             return false;
         }
     }
+    
+    /**
+    * Get the list of customers living places in the database,
+    * in order to list them on the interface.
+    *
+    * @return ArrayList of string containing the available customers
+    */
+    public static String[] getCustomerTown(){
+        String req = "SELECT customer_town from customer group by customer_town";
+        ArrayList<ArrayList> arrayResult; // creating the result ArrayList
+        arrayResult = ConnectionDB.requestStatic(req);    
+
+        String[] result = formatResult(arrayResult);
+
+       return(result);
+    }
+
+   /**
+    * Get the list of the customer that live in
+    * a defined town
+    * 
+    * @param  town  The town  (as a string)  where the customers lives 
+    * @return An arraylist of customer living in this town.
+    */
+     public static String[] getCustomerName(String town){
+        String req = "SELECT customer_name from customer WHERE customer_town ='"+ town +"'";
+        ArrayList<ArrayList> arrayResult; // creating the result ArrayList
+        arrayResult = ConnectionDB.requestStatic(req);    
+
+        String[] result = formatResult(arrayResult);
+
+       return(result);
+    }
+    
+    
 }
