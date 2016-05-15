62b07f3065135df8032674f7374958cca345bea5
willyo <willassohoun@hotmail.fr>
2016-05-15 17:08:11 +0200

    Explore Order

---
+++ Source Package/data/ExploreOrder.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author willa
+ */
+public class ExploreOrder {
+    
+    public ArrayList getOrderList(String Customer_Login){
+        return null;
+    }
+    
+    public String getAnalysisName(String OrderID){
+        return null;
+    }
+    
+    public String getOrderStatus(String OrderID){
+        return null;
+    }
+    
+    public int getTotalAnalysis(String OrderID){
+        return 0;
+    } 
+    
+    public int getAnalysisDone(String OrderID){
+        return 0;
+    }
+          
+}
