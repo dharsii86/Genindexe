c359d2f12db35c29933091470d11effa3233a9a3
willyo <willassohoun@hotmail.fr>
2016-05-15 21:02:14 +0200

    Explore Order Fini

--- Source Package/data/ExploreOrder.java
+++ Source Package/data/ExploreOrder.java
@@ -5,6 +5,7 @@
  */
 package data;
 
+import database.ConnectionDB;
 import java.util.ArrayList;
 
 /**
@@ -14,23 +15,23 @@ import java.util.ArrayList;
 public class ExploreOrder {
     
     public ArrayList getOrderList(String Customer_Login){
-        return null;
+        return ConnectionDB.requestStatic("select Order_Id from `order` where (Customer_Login = '"+Customer_Login+"');");
     }
     
     public String getAnalysisName(String OrderID){
-        return null;
+        return ConnectionDB.requestOneResult("select `Analysis_Name` from `order` where (`Order_Id`='"+OrderID+"')");
     }
     
     public String getOrderStatus(String OrderID){
-        return null;
+        return ConnectionDB.requestOneResult("select `Order_Status` from `order` where (`Order_Id`='"+OrderID+"')");
     }
     
     public int getTotalAnalysis(String OrderID){
-        return 0;
+        return Integer.parseInt(ConnectionDB.requestOneResult("SELECT count(*) FROM `order` WHERE (`Order_Status`='Done';"));
     } 
     
     public int getAnalysisDone(String OrderID){
-        return 0;
+        return Integer.parseInt(ConnectionDB.requestOneResult("SELECT count(*) FROM `order` WHERE (`Order_Status`='Done' AND `Order_Id`='"+OrderID+"');"));
     }
           
 }
