3cbd7ebce55826f71695335deb868fe15d3e0599
TheGteck <quentin.bonenfant@gmail.com>
2016-05-19 12:08:54 +0200

    Order data class comments

--- Source Package/data/OrderData.java
+++ Source Package/data/OrderData.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
 import java.util.ArrayList;
@@ -11,17 +6,35 @@ import java.util.List;
 import java.util.Set;
 import nf.*;
 /**
- *
- * @author Quentin Bonenfant
+ * This class manage the static list of orders.
+ * It is used to access order objects through the whole application.
+ * The methods used to access the list are also static.
+ * 
+ * @author SCRUM Group 2.
  */
 public class OrderData {
     
+    // Hashmap of order objects, the key is the order id (as a string).
     private static HashMap<String,SpecieCategory> categoryList;
 
+    
+    /**
+     * Getter for the static hashmap of orders.
+     * 
+     * @return The hashmap containing order objects.
+     */
     public static HashMap getCategories(){
         return(categoryList);
     }
     
+    /**
+     * Function that return the list of species that are parts of a category.
+     * It use the inner methods of order objects to return the set of species.
+     * The result is converted to String array before return.
+     * 
+     * @param cat The category of species
+     * @return The list of species as a String array
+     */
     public String[] getListSpecieFromCat(String cat){
 	ArrayList<String> res = new ArrayList<>();
 	SpecieCategory sCat = categoryList.get(cat);
@@ -34,5 +47,3 @@ public class OrderData {
         return res.toArray( result );
     }
 }
-
-
