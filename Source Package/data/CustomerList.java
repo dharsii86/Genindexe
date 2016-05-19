a1a4b289d9297b6fdf8dc7b8825457205efa380e
TheGteck <quentin.bonenfant@gmail.com>
2016-05-19 11:21:56 +0200

    Customer List class comments

--- Source Package/data/CustomerList.java
+++ Source Package/data/CustomerList.java
@@ -1,53 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
-
 import database.CustomerDB;
-import java.security.KeyException;
 import nf.Customer;
 
 /**
- *
- * @author cube
+ * This class manage the static list of customer.
+ * It is used to access customer objects through the whole application.
+ * The methods used to access the list are also static.
+ * 
+ * @author SCRUM Group 2.
  */
 public class CustomerList {
         
-    private static HashMap<String,HashMap> customerList;
-    /*This HashMap contains at first a key that correspond to the Town and allow to access another HashMap
-     *The other have for the key the name of the customer and allow to access to the object customer
+    /*This HashMap contains at first a key that correspond to the Town and
+      allows to access another HashMap. The other one have for key the name of 
+      the customer and allows to access to the customer object.
      */
+    private static HashMap<String,HashMap> customerList;
+    
 
+    /**
+     * Initialise the static list of  customer.
+     */
     public static void launchCustomerList(){
         CustomerList.customerList = new HashMap<>();
     }
     
     /**
      * Insert a new customer to the application and into the database
+     * 
      * @param cust The customer object to add
- 
      * @return Insertion result (success / fail )
      */
     public static boolean add(Customer cust){
         String name = cust.getName();
         String town =  cust.getTown();
         
-        
         if(CustomerDB.addCustomer(cust)){
             printInfoDebug();
             // if the town exist
             if(customerList.get(town)!=null){
                 HashMap nameMap = customerList.get(town);
-                System.out.println("Town exist");
-                
                 // If the customer name do not already exist in the list
                 if(nameMap.get(name)==null){
                     nameMap.put(name,cust);
@@ -55,7 +50,6 @@ public class CustomerList {
             }
             // If this is a new town
             else{
-                System.out.println("Town do not exist");
                 HashMap<String,Customer> nameMap = new HashMap<>();
                 nameMap.put(name,cust);
                 customerList.put(town, nameMap);
@@ -67,6 +61,10 @@ public class CustomerList {
         return false;  
     }
 
+    /**
+     * Print the categories in the list and their information.
+     * This function is used for debug.
+     */
     public static void printInfoDebug(){
         System.out.println("customer Information ");
         for(String k : customerList.keySet()){
@@ -82,12 +80,23 @@ public class CustomerList {
         customerList=list;
     }
     
+    /**
+     * Method to get the list of town where customer lives.
+     * 
+     * @return A string array containing the customers town.
+     */
     public static String[] getCustomerTowns(){
         Set customerSet= customerList.keySet();
         String[] townArray= (String[]) customerSet.toArray(new String[customerSet.size()]);
         return townArray;
     }
     
+    /**
+     * Method to get the user that lives in a specific town
+     * 
+     * @param town The town to search in
+     * @return The String array of customer names that live there.
+     */
     public static String[] getCustomerNameByTown(String town){
         HashMap nameMap = customerList.get(town);
         Set nameSet = nameMap.keySet();
@@ -95,10 +104,24 @@ public class CustomerList {
         return(nameArray);
     }
     
+    /**
+     * Test if a customer exist
+     * 
+     * @param name The name of the customer
+     * @param town The town of the customer
+     * @return If the customer exist. 
+     */
     public static boolean exist(String name, String town){
             return customerList.get(town).get(name) != null;
     }
     
+    /**
+     * Get the customer object from his name and town
+     * 
+     * @param name The name of the customer
+     * @param town The town of the customer
+     * @return The customer object. 
+     */
     public static Customer getCustomer(String name, String town){
         if(CustomerList.exist(name, town)){
             return (Customer) customerList.get(town).get(name);
@@ -117,6 +140,5 @@ public class CustomerList {
         s =s.toLowerCase();
         s = s.substring(0, 1).toUpperCase() + s.substring(1);
         return(s);
-    }
-    
+    } 
 }
